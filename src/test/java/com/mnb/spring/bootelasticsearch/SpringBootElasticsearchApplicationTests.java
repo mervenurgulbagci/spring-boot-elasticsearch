6fe57f370853d8411a3c39c8c295daaf6fc5a7d6
mervenurgulbagci <mervenurgulbagci@gmail.com>
2021-10-23 07:21:20 +0300

    Initial commit

---
+++ src/test/java/com/mnb/spring/bootelasticsearch/SpringBootElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnb.spring.bootelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
